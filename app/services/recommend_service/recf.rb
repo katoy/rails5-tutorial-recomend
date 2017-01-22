b4690f929ef4c5804aa9424e08053bf019c9b024
kato_youichi <youichikato@gmail.com>
2017-01-23 07:17:50 +0900

    nil アクセスが発生しないように修正

--- app/services/recommend_service/recf.rb
+++ app/services/recommend_service/recf.rb
@@ -7,14 +7,14 @@ module RecommendService
       @recfs = recfs.order(:order)
     end
 
-    def make(valid_content_hash = { book: ::Book.all, music: ::Music.all, movie: ::Movie.all })
-      prepare_target_content(valid_content_hash)
+    def make(valid_content_hash = { book: Book.all, music: Music.all, movie: Movie.all })
+      setup_target_content(valid_content_hash)
       recfs.each { |recf| recf.content = target_content(recf) }.select(&:content)
     end
 
     private
 
-    def prepare_target_content(valid_content_hash)
+    def setup_target_content(valid_content_hash)
       recf_hash = recfs.group_by(&:content_type)
       fixed_id_hash = recf_hash.map { |type, recfs| [type, recfs.map(&:fixed_id).compact.uniq] }.to_h
       valid_fixed_content_hash = { book: ::Book.all, music: ::Music.all, movie: ::Movie.all }
@@ -32,12 +32,19 @@ module RecommendService
     end
 
     def target_content(recf)
+      recf.fixed_flag ? fixed_content(recf) : possible_content(recf)
+    end
+
+    def fixed_content(recf)
       content_type = recf.content_type
-      if recf.fixed_flag
-        fixed_content_hash[content_type][recf.fixed_id]
-      else
-        possible_content_hash[content_type].shift
-      end
+      return nil if fixed_content_hash[content_type].nil?
+      fixed_content_hash[content_type][recf.fixed_id]
+    end
+
+    def possible_content(recf)
+      content_type = recf.content_type
+      return if possible_content_hash[content_type].nil?
+      possible_content_hash[content_type].shift
     end
   end
 end
